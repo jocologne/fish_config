355d95681d981c66a254700ecf7bad28ddf1a083
jocologne <pjz_2000@PC.localdomain>
2026-03-03 20:34:15 -0300

    default commit message

--- functions/gsend.fish
+++ functions/gsend.fish
@@ -1,8 +1,9 @@
 function gsend --description "git add, commit e push silencioso"
     argparse 'l/list' -- $argv; or return
     if test (count $argv) -lt 1
-        echo "Usage: gsend <msg>"
-        return 1
+        set msg "default commit message"
+    else
+        set msg (string join " " $argv)
     end
     if not git rev-parse --is-inside-work-tree >/dev/null 2>&1
         set_color --bold red
@@ -20,11 +21,10 @@ function gsend --description "git add, commit e push silencioso"
     if set -q _flag_l
         set_color cyan
         echo "Arquivos:"
-        git status --short
+        echo "$status_output"
         set_color normal
     end
-    set msg (string join " " $argv)
-    git add . >/dev/null 2>&1
+    git add -A >/dev/null 2>&1
     git commit -m "$msg" >/dev/null 2>&1; or begin
         set_color --bold red
         echo "Falha no commit"
