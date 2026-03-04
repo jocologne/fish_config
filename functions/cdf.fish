be4f8fbded8da392ce1d081bc98a290550c2c40c
jocologne <pjz_2000@PC.localdomain>
2026-03-04 12:16:12 -0300

    cdf function

--- functions/cdf.fish
+++ functions/cdf.fish
@@ -1,9 +1,18 @@
 function cdf --description "Entra em pasta com menu interativo"
-	set height (math (count (ls -d */)) + 4)
-	set choice (ls -d */ 2>/dev/null | fzf --prompt="Entrar em: " --height=$height --border)
-	if test -z "$choice"
-		echo "Cancelado."
-		return 0
-	end
-	cd $choice
+    set dirs (find . -maxdepth 1 -mindepth 1 -type d -not -name ".*" | string replace "./" "" | sort)
+    if test -z "$dirs[1]"
+        echo "Nenhuma pasta encontrada em "(pwd)
+        return 0
+    end
+    set max 15
+    set height (math (count $dirs) + 4)
+    if test $height -gt $max
+        set height $max
+    end
+    set choice (printf "%s\n" $dirs | fzf --prompt="Entrar em: " --height=$height --border)
+    if test -z "$choice"
+        echo "Cancelado."
+        return 0
+    end
+    cd $choice
 end
