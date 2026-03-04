1d4a20503e4dfcd97bc1ac20c54d885056023530
jocologne <pjz_2000@PC.localdomain>
2026-03-03 22:51:35 -0300

    upgrade col

--- functions/col.fish
+++ functions/col.fish
@@ -1,7 +1,14 @@
-function col --description "Imprime coluna especifica"
-    if test (count $argv) -ne 1
-        echo "Uso: <cmd> | col <n>"
+function col --description "Imprime colunas específicas"
+    if test (count $argv) -lt 1
+        echo "Uso: <cmd> | col <n1> <n2> ..."
         return 1
     end
-    awk "{print \$$argv[1]}"
+    set script "{print"
+    for c in $argv
+        set script "$script \$$c,"
+    end
+    set script (string replace -r ',$' '' $script)
+    set script "$script}"
+
+    awk $script
 end
