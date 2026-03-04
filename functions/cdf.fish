ca5088a164845671758ebde50023cd1418be47f7
jocologne <pjz_2000@PC.localdomain>
2026-03-04 12:24:49 -0300

    cdf function

--- functions/cdf.fish
+++ functions/cdf.fish
@@ -1,5 +1,11 @@
 function cdf --description "Entra em pasta com menu interativo"
-    set dirs (find . -maxdepth 1 -mindepth 1 -type d -not -name ".*" | string replace "./" "" | sort)
+    argparse 'o/ocultas' -- $argv
+    or return 1
+    if set -q _flag_ocultas
+        set dirs (find . -maxdepth 1 -mindepth 1 -type d | string replace "./" "" | sort)
+    else
+        set dirs (find . -maxdepth 1 -mindepth 1 -type d -not -name ".*" | string replace "./" "" | sort)
+    end
     if test -z "$dirs[1]"
         echo "Nenhuma pasta encontrada em "(pwd)
         return 0
