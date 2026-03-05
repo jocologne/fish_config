aab5e40d3cd17c4bd084dfd6e2ee6db3b2bd384a
jocologne <pjz_2000@PC.localdomain>
2026-03-04 23:25:39 -0300

    twoline prompt time_stamp

--- functions/__twoline_prompt.fish
+++ functions/__twoline_prompt.fish
@@ -2,20 +2,41 @@ function __twoline_prompt --description "Custom prompt"
 	set -l last_status $status
 	set -l cwd (prompt_pwd)
 	set -l branch (git branch --show-current 2>/dev/null)
-	#Linha de informações
+	set -l ts (date +"%H:%M:%S")
+	set -l left_plain ""
+	if test (id -u) -eq 0
+		set left_plain "🔴 "
+	else if sudo -n true 2>/dev/null
+		set left_plain "🔓 "
+	end
+	set left_plain "$left_plain$cwd"
+	if test -n "$branch"
+		set left_plain "$left_plain ($branch)"
+	end
+	# calcula espaço
+	set -l pad (math $COLUMNS - (string length $left_plain) - (string length $ts) - 2)
+	if test $pad -lt 1
+		set pad 1
+	end
+	# linha 1
 	if test (id -u) -eq 0
 		echo -n '🔴 '
 	else if sudo -n true 2>/dev/null
 		echo -n '🔓 '
 	end
 	set_color cyan
-	echo -n "$cwd "
+	echo -n $cwd
+
 	if test -n "$branch"
 		set_color magenta
-		echo -n "($branch) "
+		echo -n " ($branch)"
 	end
-	echo ""
-	#Linha de prompt
+	# espaço + relógio
+	set_color normal
+	printf "%*s" $pad ""
+	set_color yellow
+	echo $ts
+	# linha 2
 	if test $last_status -eq 0
 		set_color green
 		echo -n "❯❯❯ "
